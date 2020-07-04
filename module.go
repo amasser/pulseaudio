3e22c8f7b78690f901acb254b4f8e36dd762fa0f
lawl <github@dumbinter.net>
2020-07-04 16:51:42 +0200

    Add module loading and unloading support

--- module.go
+++ module.go
@@ -43,3 +43,30 @@ func (c *Client) ModuleList() ([]Module, error) {
 	}
 	return modules, nil
 }
+
+// UnloadModule requests pulseaudio to unload the module with the specified index.
+// The index can be found e.g. with ModuleList()
+func (c *Client) UnloadModule(index uint32) error {
+	_, err := c.request(commandUnloadModule,
+		uint32Tag, index)
+	return err
+}
+
+// LoadModule requests pulseaudio to load the module with the specified name and argument string.
+// More information on how to supply these can be found in the pulseaudio documentation:
+// https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#loadablemodules
+// e.g. LoadModule("module-alsa-sink", "sink_name=headphones sink_properties=device.description=Headphones")
+// would be equivalent to the pulse config directive: load-module module-alsa-sink sink_name=headphones sink_properties=device.description=Headphones
+// Returns the index of the loaded module or an error
+func (c *Client) LoadModule(name string, argument string) (index uint32, err error) {
+	var idx uint32
+	r, err := c.request(commandLoadModule,
+		stringTag, []byte(name), byte(0), stringTag, []byte(argument), byte(0))
+
+	if err != nil {
+		return 0, err
+	}
+
+	err = bread(r, uint32Tag, &idx)
+	return idx, err
+}
